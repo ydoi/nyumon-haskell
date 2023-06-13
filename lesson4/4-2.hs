afb0e022d6706b2410bd63c4ad32632ae4b51d62
土居　祐貴 <doikun012@icloud.com>
2023-06-13 21:08:33 +0900

    answer

---
+++ lesson4/4-2.hs
@@ -0,0 +1,22 @@
+sfOffice name = if lastName < "L"
+                then nameText
+                    ++ " - PO Box 1234 - San Francisco, CA, 94111"
+                else nameText
+                    ++ " - PO Box 1010 - San Francisco, CA, 94109"
+        where lastName = snd name
+              nameText = (fst name) ++ " " ++ lastName
+
+nyOffice name = nameText ++ ": PO Box 789 - New York, NY, 10013"
+  where nameText = (fst name) ++ " " ++ (snd name)
+
+renoOffice name = nameText ++ " - PO Box 456 - Reno, NV 89523"
+  where nameText = (fst name) ++ " " ++ (snd name)
+
+dcOffice name = nameText ++ " - PO Box ??? - Dc, ?? ?????"
+  where nameText = (fst name) ++ (snd name) ++ "Esq"
+getLocationFunction location = case location of
+    "ny" -> nyOffice
+    "sf" -> sfOffice
+    "reno" -> renoOffice
+    "dc" -> dcOffice
+    _ -> (\name -> (fst name) ++ " " ++ (snd name))
